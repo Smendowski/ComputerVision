cbfa76baf78b82aa9c0fe3c41ab8bc043161cc54
Smendowski <m.smendowski@gmail.com>
2020-04-30 19:28:13 +0200

    Optimization

--- 11. Find Lanes for Self-Driving Cars.py
+++ 11. Find Lanes for Self-Driving Cars.py
@@ -2,6 +2,34 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def make_coordinates(image, line_parmeters):
+	slope, intercept = line_parmeters
+	y1 = image.shape[0]
+	y2 = int(y1*(3/5))
+	x1 = int((y1 - intercept)/slope)
+	x2 = int((y2 - intercept)/slope)
+	return np.array([x1, y1, x2, y2])
+
+
+def average_slope_intercept(image, lines):
+	left_fit = []
+	right_fit = []
+	for line in lines:
+		x1,y1,x2,y2 = line.reshape(4)
+		parameters = np.polyfit((x1,x2), (y1,y2), 1)
+		slope = parameters[0]
+		intercept = parameters[1]
+		if slope < 0:
+			left_fit.append((slope, intercept))
+		else:
+			right_fit.append((slope, intercept))
+	left_fit_average = np.average(left_fit, axis=0)
+	right_fit_average = np.average(right_fit, axis=0)
+	left_line = make_coordinates(image, left_fit_average)
+	right_line = make_coordinates(image, right_fit_average)
+	return np.array([left_line, right_line])
+
+
 image = cv2.imread("images/Self-DrivingCars/CarLanes1.png")
 # 1st step - Convert image to grayscale
 # GrayScale image = 1 channel processing is faster than 3 channels BGR
@@ -48,6 +76,8 @@ def display_lines(images, lines):
 
 lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100,
 	np.array([]), minLineLength=40, maxLineGap=5)
+
+avaraged_lines = avarage_slope_intercept(lane_image, lines)
 
 line_image = display_lines(lane_image, lines)
 combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
